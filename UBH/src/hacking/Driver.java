b383736b5977765f36b52b41794fba39722823d5
Nicholas Wheeler <nhwheele@buffalo.edu>
2013-04-13 20:04:20 -0400

    it works

---
+++ UBH/src/hacking/Driver.java
@@ -0,0 +1,22 @@
+package hacking;
+
+import java.io.IOException;
+
+
+public class Driver {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		Parse p = new Parse();
+		p.total();
+		p.tax();
+		System.out.println(p.total());
+		System.out.println(p.tax());
+		FiletoExcel fe = new FiletoExcel();
+		fe.excel(p.total(), p.tax());
+	}
+
+}
--- UBH/src/hacking/FiletoExcel.java
+++ UBH/src/hacking/FiletoExcel.java
@@ -1,17 +1,21 @@
-package src;
+package hacking;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class FiletoExcel {
-	public static void main(String[] args) throws IOException{
-		FileWriter fwriter = new FileWriter("demo.csv");
+	
+	public void excel(String total, String tax) throws IOException{
+	
+		FileWriter fwriter = new FileWriter("de.csv");
 		PrintWriter outputFile = new PrintWriter(fwriter);
 		
 		String name = "John Doe";
-		double amountOfSale = 100;
-		double amountofTax= 8.75;
+		double tot = Double.parseDouble(total);
+		double ta = Double.parseDouble(tax);
+		double amountOfSale = tot;
+		double amountofTax= ta;
 		outputFile.println("Name, Total, Taxed");
 		outputFile.println(name + ","+amountOfSale+","+amountofTax);
 		outputFile.close();
